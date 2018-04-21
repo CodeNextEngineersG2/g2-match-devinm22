ea2a2c7934de02e79c1e36aa8b64cb6a1e08778d
Student <student@Admins-MacBook-Air.local>
2018-04-21 12:48:33 -0700

    making game show up on website

--- script.js
+++ script.js
@@ -45,10 +45,10 @@ var resetButton, musicButton;
 function loadImages() {
    backImage = loadImage("assets/img/back.png");
    boltImage = loadImage("assets/img/bolt.png");
-   cloudIage = loadImage("assets/img/cloud.png");
+   cloudImage = loadImage("assets/img/cloud.png");
    sunImage = loadImage("assets/img/sun.png");
    moonImage = loadImage("assets/img/moon.png");
-   smileyImage = loadImage("assets/img/smile.png");
+   smileyImage = loadImage("assets/img/smiley.png");
    heartImage = loadImage("assets/img/heart.png");
    transitionImage1 = loadImage("assets/img/transition1.png");
    transitionImage2 = loadImage("assets/img/transition2.png");
@@ -116,12 +116,25 @@ function setup(){
   imageArray = [ backImage, boltImage, cloudImage, sunImage, moonImage, smileyImage, heartImage,
                  transitionImage1, transitionImage2, transitionImage3];
   resizeImages();
+  createSprites();
+  spriteArray = [boltSprite1, boltSprite2, cloudSprite1, cloudSprite2,
+    sunSprite1, sunSprite2,  moonSprite1, moonSprite2, smileySprite1, smileySprite2,
+    heartSprite1, heartSprite2];
+    addAnimations();
+    shuffle(spriteArray, true);
+    placeSprites();
+    spritesActive = true;
+
+
 }
 
 /*
  * function draw()
  */
-
+function draw() {
+  background(20, 40, 60);
+  drawSprites();
+}
 /*
  * function init()
  * Initializes various elements of the game. Called in both setup() and
@@ -160,7 +173,6 @@ function resizeImages(){
   }
 }
 
-
 /*
  * function createSprites()
  * Initializes each sprite variable (e.g., sunSprite1) as a sprite object
@@ -173,7 +185,20 @@ function resizeImages(){
      mySprite = createSprite(0, 0, spriteWidth, spriteHeight);
    }
  */
-
+function createSprites() {
+  boltSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+  boltSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+  cloudSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+  cloudSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+  sunSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+  sunSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+  moonSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+  moonSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+  smileySprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+  smileySprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+  heartSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+  heartSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+}
 
 /*
  * function addAnimations()
@@ -183,7 +208,18 @@ function resizeImages(){
  * each animation's frameDelay, loop, and playing properties. Finally, this
  * function calls activateSprite(s) with each sprite as input.
  */
-
+function addAnimations() {
+  var animations = [boltAnimation, boltAnimation, cloudAnimation, cloudAnimation,
+                  sunAnimation, sunAnimation, moonAnimation, moonAnimation,
+                  smileyAnimation, smileyAnimation,
+                  heartAnimation, heartAnimation];
+  for(var i = 0; i < spriteArray.length; i++) {
+    spriteArray[i].addAnimation("flip", animations[i]);
+    spriteArray[i].animation.frameDelay = 10;
+    spriteArray[i].animation.looping = false;
+    spriteArray[i].animation.playing = false;
+  }
+}
 
 /*
  * function placeSprites()
@@ -191,7 +227,19 @@ function resizeImages(){
  * pattern you like. For starters, try arranging the sprites in a simple
  * grid-like pattern (e.g., 2x2 if you only have four sprites).
  */
-
+function placeSprites(){
+  for(var i = 0; i < spriteArray.length; i++){
+    spriteArray[i].position.x = spriteX;
+    spriteArray[i].position.y = spriteY;
+    if((i + 1) % 6==0 ) {// if the number of sprites is divisble by 6
+      spriteX = 70;
+      spriteY += spriteHeight + 10;
+    }
+    else {
+      spriteX += spriteWidth + 10;
+    }
+  }
+}
 
 /*
  * function activateSprite(s)
